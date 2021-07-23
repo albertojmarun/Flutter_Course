c188d8bedc1650d09ea37778ba9cf8416e821e4e
albertojmarun <albertojmarun@gmail.com>
2021-07-23 20:49:29 +0200

    Flutter - Container
    This part of the Tutorial, talk about the containers itself, and how to see which parameters needs the Widget itself, how to declare it, use it, happens...
    
    * There was a problem with the Inspector, so, I decide to make a new project, but keeping the same idea as before, that is why are changes on:
    - .metada
    - project.pbxproj
    - .gitignore
    - widget_test.dart

--- ejemplos/lib/main.dart
+++ ejemplos/lib/main.dart
@@ -1,19 +1,55 @@
 // IMPORTS GO AT THE BEGINNING.
 import 'package:flutter/material.dart';
 
-class myApp extends StatelessWidget {
-  const myApp({Key? key}) : super(key: key);
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Ejemplos Flutter',
       theme: ThemeData(primarySwatch: Colors.green),
       home: Scaffold(
-        appBar: AppBar(title: Text('Ejemplos Flutter')),
+        appBar: AppBar(
+          title: Text('Ejemplos Flutter'),
+        ),
         body: Center(
-          child: Text(
-            'Hello Flutter App in a Function',
+          child: Container(
+            // width: 200,
+            // height: 200,
+            // You cannot combine a color and a decoration.
+            // color: Colors.red,
+            // decoration: FlutterLogoDecoration(),
+            decoration: BoxDecoration(
+              color: Colors.pink[50],
+              border: Border.all(
+                color: Colors.red,
+                width: 6,
+                style: BorderStyle.solid,
+              ),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(15),
+                topRight: Radius.circular(15),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black,
+                  offset: Offset(4, 4),
+                  blurRadius: 5,
+                ),
+              ],
+            ),
+            margin: EdgeInsets.all(10),
+            child: Text(
+              'Hola Bebe' + ', Como andas?',
+              style: TextStyle(fontSize: 30),
+            ),
+            padding: EdgeInsets.all(20),
+            // padding: EdgeInsets.only(left: 20),
+            constraints: BoxConstraints(
+              maxWidth: 300,
+              maxHeight: 400,
+              minWidth: 100,
+              minHeight: 150,
+            ),
           ),
         ),
       ),
@@ -22,5 +58,5 @@ class myApp extends StatelessWidget {
 }
 
 void main() {
-  runApp(myApp());
+  runApp(MyApp());
 }
