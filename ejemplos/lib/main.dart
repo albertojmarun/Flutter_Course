9aa14b001c71a2952c3b49fa6c40b33a6b72ab98
albertojmarun <albertojmarun@gmail.com>
2021-07-24 17:18:55 +0200

    Flutter - Padding
    There is a study to the Padding Widget.
    How it works, and the parameters inside, like:
    - padding
    - child
    - key

--- ejemplos/lib/main.dart
+++ ejemplos/lib/main.dart
@@ -1,44 +1,18 @@
 // IMPORTS GO AT THE BEGINNING.
 import 'package:flutter/material.dart';
 
-// Flutter - Text
+// Flutter - Padding
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Center(
-        child: SizedBox(
-          width: 350,
-          height: 150,
-          child: Text(
-            'Un texto ' * 10,
-            textAlign: TextAlign.center,
-            textDirection: TextDirection.ltr,
-            textScaleFactor: 1.2,
-            maxLines: 3,
-            overflow: TextOverflow.visible,
-            style: TextStyle(
-              color: Colors.yellow,
-              fontSize: 25,
-              fontWeight: FontWeight.w900,
-              fontStyle: FontStyle.italic,
-              decoration: TextDecoration.overline,
-              decorationColor: Colors.red,
-              decorationThickness: 1.4,
-              decorationStyle: TextDecorationStyle.wavy,
-              backgroundColor: Colors.purple,
-              shadows: [
-                Shadow(
-                  color: Colors.blue,
-                  offset: Offset(4, 4),
-                  blurRadius: 4,
-                ),
-              ],
-              letterSpacing: 5,
-              wordSpacing: 50,
-              height: 1.5,
-            ),
-          ),
+      home: Padding(
+        padding: EdgeInsets.symmetric(
+          vertical: 20,
+          horizontal: 30,
+        ),
+        child: Container(
+          color: Colors.orange,
         ),
       ),
     );
