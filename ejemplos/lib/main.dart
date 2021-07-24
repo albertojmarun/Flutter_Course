24f238a22699e4cf7ec9c05d4be8641b0f246777
albertojmarun <albertojmarun@gmail.com>
2021-07-24 20:05:49 +0200

    Flutter - Column
    Explains how the columns work.
    Parameters Inside:
    - children
    - mainAxisSize
    - mainAxisAlignment
    - crossAxisAlignment
    - verticalDirection
    
    Which precede other one.
    Inside the children of the column, there has to be an array of widget (Can be whatever Widget).

--- ejemplos/lib/main.dart
+++ ejemplos/lib/main.dart
@@ -1,18 +1,44 @@
 // IMPORTS GO AT THE BEGINNING.
 import 'package:flutter/material.dart';
 
-// Flutter - Padding
+// Flutter - Column
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Padding(
-        padding: EdgeInsets.symmetric(
-          vertical: 20,
-          horizontal: 30,
-        ),
-        child: Container(
-          color: Colors.orange,
+      home: Container(
+        color: Color.fromARGB(255, 230, 240, 255),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text('Primera'),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.green,
+              ),
+              Text(
+                'Segunda',
+                style: TextStyle(fontSize: 20),
+                // textAlign: TextAlign.center,
+              ),
+              Container(
+                height: 150,
+                width: 120,
+                color: Colors.blue,
+              ),
+            ],
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+            /// CROSS AXIS ALIGNMENT PRECEDE TEXT ALIGN ON TEXT.
+            crossAxisAlignment: CrossAxisAlignment.center,
+
+            /// VERTICAL DIRECTION
+            /// VerticalDirection.up -> starts at the end, and finish at the beginning.
+            /// VerticalDirection.down -> default.
+            verticalDirection: VerticalDirection.down,
+          ),
         ),
       ),
     );
